746de7586523a9cf266f3114ce063cf3b9f51168
Den <115546299+Denthor99@users.noreply.github.com>
2023-05-24 22:01:34 +0200

    drodsan2708 - creación de directorio ScriptSQL + subida scripts varios

---
+++ ScriptsSQL/insercion alumnos.sql
@@ -0,0 +1,21 @@
+# Insertamos los valores para cada campo de la tabla
+USE instituto;
+INSERT INTO daw1 VALUES
+(1, 'Víctor', 0, NULL),
+(2, 'John', 0, NULL),
+(3, 'Jorge', 2, '2023-05-22'),
+(4, 'Julián', 1, '2023-05-05'),
+(5, 'Diego', 0, NULL),
+(6, 'David', 0, NULL),
+(7, 'Juanma Herrera', 3, '2023-05-22'),
+(8, 'Jonathan', 3, '2023-05-19'),
+(9, 'Pablo', 1, '2023-05-05'),
+(10, 'Juan María', 1, '2023-05-17'),
+(11, 'Adrián', 1, '2023-05-24'),
+(12, 'Ricardo', 2, '2023-05-15'),
+(13, 'Juan Jesús', 0, NULL),
+(14, 'Pepe', 0, NULL),
+(15, 'Dani', 2, '2023-05-22'),
+(16, 'Juanma Saborido', 0, NULL),
+(17, 'Antonio', 1, '2023-05-15'),
+(18, 'Jossie', 0, NULL);
